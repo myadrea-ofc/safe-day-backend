913dc570bd7447b5586717c1059d80c5c83a5f17
myadrea-ofc <melvin.andrea@bagasbumipersada.co.id>
2026-02-10 13:51:51 +0700

    test

--- routes/notification.routes.js
+++ routes/notification.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const pool = require("../../config/db");
+const pool = require("../config/db");
 const authMiddleware = require("../../middlewares/auth.middleware");
 
 router.get("/unread-count", authMiddleware, async (req, res) => {
