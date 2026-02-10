3a1a0bd6cb088698f483c08286e144e73b03110d
myadrea-ofc <melvin.andrea@bagasbumipersada.co.id>
2026-02-10 13:53:02 +0700

    yang ini beneran kok

--- routes/notification.routes.js
+++ routes/notification.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const pool = require("../config/db");
-const authMiddleware = require("../../middlewares/auth.middleware");
+const authMiddleware = require("../middlewares/auth.middleware");
 
 router.get("/unread-count", authMiddleware, async (req, res) => {
   const result = await pool.query(
