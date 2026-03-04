ff84d05f16ec60aa016dde6b59b91e3fc8cad11c
myadrea-ofc <melvin.andrea@bagasbumipersada.co.id>
2026-03-04 17:02:13 +0700

    fix routes and add excel js

--- controllers/excel.access.controller.js
+++ controllers/excel.access.controller.js
@@ -1,4 +1,4 @@
-const pool = require("../db");
+const pool = require("../config/db");
 
 // ==============================
 // GET LIST ACCESS
--- routes/excelaccess.routes.js
+++ routes/excelaccess.routes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const controller = require("../controllers/excelAccessController");
-const auth = require("../middleware/auth");
+const controller = require("../controllers/excel.access.controller");
+const auth = require("../middlewares/auth.middleware");
 
 router.get("/", auth, controller.getAccessList);
 router.post("/grant", auth, controller.grantAccess);
